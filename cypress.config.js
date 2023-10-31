a14c855245bdb6f78caf6781185dece416b5e713
Euller Lisowski <eullersl@gmail.com>
2023-10-31 12:06:31 -0300

    Adiciona configurações do cypress/mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,23 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
-  },
-});
+    baseUrl: 'http://localhost:3000',
+    video: false,
+    screenshotOnRunFailure: false,
+    defaultCommandTimeout: 250000,
+    requestTimeout: 250000,
+    responseTimeout: 250000,
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      inlineAssets: true,
+      charts: true,
+      consoleReporter: 'spec',
+      reportFilename: "index",
+      overwrite: true,
+      html: false,
+      json: true
+    }
+  }
+})
